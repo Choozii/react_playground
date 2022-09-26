c71a9475c15086d824d9f2f7d547d785f79ce0fb
choozii <jiyechoo@gmail.com>
2022-09-26 18:57:45 +0900

    nextjs practice

--- nextjs-course-code/components/events/List.jsx
+++ nextjs-course-code/components/events/List.jsx
@@ -1,7 +1,7 @@
-import classes from './List.module.css';
+import classes from "./List.module.css";
 import React from "react";
 import Card from "./Card";
-const List = ({ events}) => {
+const List = ({ events }) => {
   return (
     <div className={classes.list}>
       {events.map((event) => (
